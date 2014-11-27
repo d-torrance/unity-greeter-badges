9d2c6d0f91e7b7c743092d73898be8630aefbdef
Doug Torrance <dtorrance@monmouthcollege.edu>
2014-11-26 21:13:12 -0600

    Remove unnecessary dh_make-generated files.

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(unity-greeter-badges):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="unity-greeter-badges" command="/usr/bin/unity-greeter-badges"
--- debian/unity-greeter-badges.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the unity-greeter-badges package
-#
-0 4	* * *	root	[ -x /usr/bin/unity-greeter-badges_maintenance ] && /usr/bin/unity-greeter-badges_maintenance
